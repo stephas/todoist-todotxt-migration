0b484f14b2514d83677bf75db0050e24ba8dd2ab
Stephan <960085+stephas@users.noreply.github.com>
2022-12-05 18:30:56 -0800

    rename library

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='todoist-todotxt-migration',
-    packages=find_packages(include=['todotxt_export']),
+    packages=find_packages(include=['todoist_todotxt_migration']),
     version='0.1.0',
     description='My first Python library',
     author='Me',
