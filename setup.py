81ac8a6291309695b8e0ffaf99d89d64c09d0363
Stephan <960085+stephas@users.noreply.github.com>
2022-12-05 18:34:39 -0800

    bump library version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='todoist-todotxt-migration',
     packages=find_packages(include=['todoist_todotxt_migration']),
-    version='0.1.0',
+    version='0.1.1',
     description='My first Python library',
     author='Me',
     license='MIT',
