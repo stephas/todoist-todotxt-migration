6848e50275d843e16bcc8fa2e55fca1bd4939ce0
Stephan <960085+stephas@users.noreply.github.com>
2022-12-20 20:16:45 +0000

    integ test

---
+++ tests/test_integ.py
@@ -0,0 +1,23 @@
+import os
+
+import pytest
+
+from todoist_todotxt_migration.tools import Migration, ProjectRenameStrategy
+
+@pytest.fixture
+def filename():
+    name = 'todo_integ.txt'
+    try:
+        os.remove(name)
+    except FileNotFoundError:
+        pass
+    yield name
+    os.remove(name)
+
+
+def test_integ(filename):
+    migration = Migration.from_env_secret()
+    migration.generate_file(filename)
+    migration.clear_cache()
+    with open(filename, 'r') as fh:
+        assert len(fh.readlines()) > 0
