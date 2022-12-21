83ae65403182bed3633640e7f61c79054421843e
Stephan <960085+stephas@users.noreply.github.com>
2022-12-21 21:43:52 +0000

    version 0.2.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='todoist-todotxt-migration',
     packages=find_packages(include=['todoist_todotxt_migration']),
-    version='0.2.0',
+    version='0.2.1',
     description='My first Python library',
     author='Me',
     license='MIT',
