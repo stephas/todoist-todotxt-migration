bfd9c86d3c1329eb2a0f6ee715595a88ed662398
Stephan <960085+stephas@users.noreply.github.com>
2022-12-21 21:09:44 +0000

    shared test utilities

--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,62 @@
-from utils import make_todoist_task, make_todoist_project, make_new_test_case, make_project_map, migration
+import pytest
 
 from todoist_todotxt_migration.tools import Migration, ProjectRenameStrategy
 from todoist_api_python.models import Task, Project
 
 # TBD: test creation date missing
 
+def make_todoist_project(name, id=None, parent_id=None):
+    return Project.from_dict({
+        "color": None,
+        "id": id,
+        "parent_id": parent_id,
+        "comment_count": None,
+        "is_favorite": None,
+        "is_shared": None,
+        "name": name,
+        "url": None,
+        "view_style": None,
+    })
+
+def make_todoist_task(content, priority, is_completed=False, project_id=None, labels=None):
+    return Task.from_dict({
+        "comment_count": None,
+        "is_completed": is_completed,
+        "content": content,
+        "created_at": "2022-12-20T",
+        "creator_id": None,
+        "description": None,
+        "id": None,
+        "priority": priority,
+        "project_id": project_id,
+        "section_id": None,
+        "url": None,
+        "labels": labels
+    })
+    
+
+def make_new_test_case(migration, todoist_task, todoist_project):
+    todoist_project.id = 1
+    todoist_task.project_id = todoist_project.id
+
+    migration.get_project_by_id_map = lambda: {todoist_project.id: todoist_project}
+
+    return migration
+
+def make_project_map(migration, projects):
+    m = {}
+    for p in projects:
+        m[p.id] = p
+    migration.get_project_by_id_map = lambda: m
+    return m
+
+
+@pytest.fixture
+def migration():
+    m = Migration(token=None)
+    m.get_project_by_id_map = lambda: {}
+    return m
+    
 def test_task_without_project(migration):
     t = make_todoist_task("A task", priority=1)
 
